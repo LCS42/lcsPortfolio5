6a6e251cd02dacaa0d6d1dc316b04a2bb217f43d
LCS42 <114628502+LCS42@users.noreply.github.com>
2022-09-30 21:44:49 -0400

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Soccer' },
+  { id: 13, name: 'Football' },
+  { id: 14, name: 'Cross Country' },
+  { id: 15, name: 'Tennis' },
+  { id: 16, name: 'Volleyball' },
+  { id: 17, name: 'Baseball' },
+  { id: 18, name: 'Softball' },
+  { id: 19, name: 'Wrestling' },
+  { id: 20, name: 'Basketball' }
+];
